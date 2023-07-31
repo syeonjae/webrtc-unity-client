70f1ab69c9bf7329ca4f42c860a26dc904cdcb76
syeonjae <syeonjae@fakeeyes.co.kr>
2023-07-31 23:54:57 +0900

    test#4

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,11 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 using Unity.WebRTC;
-using WebSocketSharp;
-using System;
-using System.Text;
 using UnityEngine.UI;
+
+using WebSocketSharp;
+
+using LitJson;
+using System.Text;
+using System.Collections.Generic;
 
 internal static class WebRTCSettings
 {
@@ -46,6 +49,21 @@ public class Data
     public RTCSessionDescription answer;
     public RTCIceCandidate candidate;
     public string mid;
+
+    public override string ToString()
+    {
+        return $"Data.ToString()\n" +
+                $"\ntype: {type}\n" +
+                $"name: {name}\n" +
+                $"room: {room}\n" +
+                $"success: {success}\n" +
+                $"offer.type: {offer.type}\n" +
+                $"offer.sdp: {offer.sdp}\n" +
+                $"answer.type: {answer.type}\n" +
+                $"answer.sdp: {answer.sdp}\n" +
+                $"candidate: {candidate}\n" +
+                $"mid: {mid}";
+    }
 }
 
 
@@ -58,13 +76,13 @@ public class test : MonoBehaviour
     private DelegateOnIceCandidate connIceCandidate;
 
     // Start is called before the first frame update
-    private string IP = "192.168.0.165";
+    private string IP = "192.168.1.143";
     private string PORT = "8080";
 
     public WebSocket ws = null;
 
     private RTCPeerConnection pc;
-    private MediaStream videoStream;
+    private MediaStream videoStream = null;
     private string target; // 타겟이 있다면 항상 admin에게
 
 
@@ -81,7 +99,8 @@ public class test : MonoBehaviour
         WebRTC.Initialize(WebRTCSettings.LimitTextureSize);
     }
 
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         WebRTC.Dispose();
     }
 
@@ -123,11 +142,12 @@ public class test : MonoBehaviour
     {
         try
         {
-            if(ws == null || !ws.IsAlive)
+            if (ws == null || !ws.IsAlive)
             {
                 ws.Close();
             }
-        } catch (Exception e)
+        }
+        catch (Exception e)
         {
             Debug.Log(e.ToString());
         }
@@ -149,18 +169,19 @@ public class test : MonoBehaviour
         Debug.Log("DisconnectServer");
         try
         {
-            if(ws == null)
+            if (ws == null)
             {
                 return;
             }
 
-            if(ws.IsAlive)
+            if (ws.IsAlive)
             {
                 pc.Close();
                 pc.Dispose();
                 pc = null;
             }
-        } catch (Exception)
+        }
+        catch (Exception)
         {
             throw;
         }
@@ -170,78 +191,128 @@ public class test : MonoBehaviour
 
     public void Recv(object sender, MessageEventArgs e)
     {
-        MainThreadHelper.AddAction(() =>
+        try
         {
-            try
+            Data recvData = JsonUtility.FromJson<Data>(e.Data);
+            Debug.Log($"Recv.Data: {e.Data}");
+            Debug.Log($"recvData: {recvData}");
+
+            switch (recvData.type)
             {
-                Data recvData = JsonUtility.FromJson<Data>(e.Data);
-                switch (recvData.type)
-                {
-                    case "login":
-                        handleLogin(recvData.success);
-                        break;
-                    case "offer":
-                        Debug.Log("recvData.type");
-                        Debug.Log(recvData.type);
-                        Debug.Log("recvData.offer");
-                        Debug.Log(recvData.offer);
-                        handleOffer(recvData.offer, recvData.name);
-                        break;
-                    case "answer":
-                        handleAnswer(recvData.answer);
-                        break;
-                    case "candidate":
-                        handleCandidate(recvData.candidate);
-                        break;
-                    case "leave":
-                        handleLeave();
-                        break;
-                }
+                case "login":
+                    handleLogin(recvData.success);
+                    break;
+                case "offer":
+                    RTCSessionDescription offer = new RTCSessionDescription()
+                    {
+                        type = RTCSdpType.Offer,
+                        sdp = JsonMapper.ToObject(e.Data)["offer"]["sdp"].ToString(),
+                    };
+                    handleOffer(offer, recvData.name);
+                    break;
+                case "answer":
+                    handleAnswer(recvData.answer);
+                    break;
+                case "candidate":
+                    JsonData data = JsonMapper.ToObject(e.Data)["candidate"];
+                    string candidate = data[nameof(candidate)].ToString();
+                    string sdpMid = data[nameof(sdpMid)].ToString();
+                    int sdpMLineIndex = int.Parse(data[nameof(sdpMLineIndex)].ToString());
+                    //string userNameFragment = data[nameof(userNameFragment)].ToString();
+
+                    RTCIceCandidate _candidate = new RTCIceCandidate(new RTCIceCandidateInit()
+                    {
+                        candidate = candidate,
+                        sdpMid = sdpMid,
+                        sdpMLineIndex = sdpMLineIndex,
+                    });
+                    handleCandidate(_candidate);
+
+                    //handleCandidate(recvData.candidate);
+                    break;
+                case "leave":
+                    handleLeave();
+                    break;
             }
-            catch (Exception error)
-            {
-                Debug.LogError($"Message: {error.Message}\nStackTrace: {error.StackTrace}");
-                throw error;
-            }
-        });
+        }
+        catch (Exception error)
+        {
+            Debug.LogError($"Message: {error.Message}\nStackTrace: {error.StackTrace}");
+            throw error;
+        }
     }
 
-    public void handleLogin(bool success)
+    public async void handleLogin(bool success)
     {
-        if(!success)
+        if (!success)
         {
             Debug.Log("로그인 실패!! 다시 시도해주세요");
-        } else
+        }
+        else
         {
             Debug.Log("로그인 성공");
             var configuration = pc_config();
 
             pc = new RTCPeerConnection(ref configuration);
 
-            if (videoStream == null)
+            videoStream = null;
+            MainThreadHelper.AddAction(() =>
             {
                 videoStream = cam.CaptureStream(WebRTCSettings.StreamSize.x, WebRTCSettings.StreamSize.y, 1000000);
-            }
+                sourceImage.texture = cam.targetTexture;
+                sourceImage.color = Color.white;
+            });
 
-            sourceImage.texture = cam.targetTexture;
-            sourceImage.color = Color.white;
+            await System.Threading.Tasks.Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (videoStream != null)
+                        break;
+                }
+            });
+
+            Debug.Log($"find videoStream finished");
+
+
 
             // add Track
+            var pcVideoSenders = new List<RTCRtpSender>();
             foreach (var track in videoStream.GetTracks())
             {
-                pc.AddTrack(track, videoStream);
+                var pcSender = pc.AddTrack(track, videoStream);
+
+                if(track.Kind == TrackKind.Video)
+                {
+                    pcVideoSenders.Add(pcSender);
+                }
+            }
+
+            if(WebRTCSettings.UseVideoCodec != null)
+            {
+                var codecs = new[] { WebRTCSettings.UseVideoCodec };
+                foreach (var transceiver in pc.GetTransceivers())
+                {
+                    if (pcVideoSenders.Contains(transceiver.Sender))
+                    {
+                        transceiver.SetCodecPreferences(codecs);
+                    }
+                }
             }
 
             pc.OnIceCandidate = candidate =>
             {
-                try {
+                try
+                {
                     target = "admin";
                     Data data = new Data();
                     data.type = "candidate";
                     data.room = "test";
                     data.candidate = candidate;
                     SendTo(data);
-                } catch (Exception error) {
+                }
+                catch (Exception error)
+                {
                     Debug.Log("OnIceCandidate");
                     Debug.Log(error);
                 }
@@ -249,49 +320,77 @@ public class test : MonoBehaviour
         }
     }
 
-    public void handleOffer(RTCSessionDescription offer, string name)
+    public async void handleOffer(RTCSessionDescription offer, string name)
     {
-        Debug.Log("offer");
-        Debug.Log(offer);
-        Debug.Log("유저가 오퍼를 받음");
+        Debug.Log($"handOffer. (offer == default) == {offer.Equals(default(RTCSessionDescription))}");
+        Debug.Log($"handleOffer\nname: {name}\ntype: {offer.type}\nsdp: {offer.sdp}");
+
         target = name;
         pc.SetRemoteDescription(ref offer);
-        Debug.Log("유저가 offer를 setRemoteDescription에 등록했습니다.");
 
         var answer = pc.CreateAnswer();
+        Debug.Log($"CreateAnswer");
+        await System.Threading.Tasks.Task.Run(() =>
+        {
+            while (true)
+            {
+                if (answer.IsDone)
+                {
+                    Debug.Log($"CreateAnswer break");
+                    break;
+                }
+            }
+        });
+        Debug.Log($"CreateAnswer done");
         onCreateAnswerSuccess(pc, answer.Desc);
     }
 
     void onCreateAnswerSuccess(RTCPeerConnection pc, RTCSessionDescription answer)
     {
         pc.SetLocalDescription(ref answer);
+        target = "admin";
+        RTCSessionDescription _answer = new RTCSessionDescription()
+        {
+            sdp = answer.sdp,
+            type = answer.type,
+        };
+
         Data data = new Data();
-        data.type = "answer";
-        data.answer = answer;
+        data.answer = _answer;
+        data.name = "admin";
         data.room = "test";
-        SendTo(data);
+        data.type = "answer";
+
+        string jsonData = JsonMapper.ToJson(data);
+        ws.Send(jsonData);
+        Debug.Log("answer을 어드민에게 전달");
     }
 
     public void handleAnswer(RTCSessionDescription answer)
     {
+        Debug.Log("handleAnswer");
         //
     }
 
     public void handleCandidate(RTCIceCandidate candidate)
     {
+        Debug.Log("handleCandidate");
+        Debug.Log($"pc null: {pc == null}");
         //
-        pc.OnIceCandidate(candidate);
-        Debug.Log("유저가 ICE candidate 등록");
+        pc.AddIceCandidate(candidate);
+        //Debug.Log("유저가 ICE candidate 등록");
     }
 
     public void handleLeave()
     {
+        Debug.Log("handleLeave");
         //
     }
 
 
     public void SendTo(Data message)
     {
+        Debug.Log($"SendTo. message: {message.name}");
         if (!ws.IsAlive)
         {
             return;
